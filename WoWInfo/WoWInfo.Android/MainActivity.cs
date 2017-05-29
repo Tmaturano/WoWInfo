d806ef7a2bfe58652b502100db4ccbb3e58ee750
Thiago Maturana de Oliveira <thiagomatu@hotmail.com>
2017-05-29 11:54:52 -0300

    Implemented push notification for Android with Google Cloud Message and Azure

--- WoWInfo/WoWInfo.Android/MainActivity.cs
+++ WoWInfo/WoWInfo.Android/MainActivity.cs
@@ -6,14 +6,30 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Gcm.Client;
+using MonkeyHubApp.Droid.Services;
 
 namespace WoWInfo.Droid
 {
     [Activity(Label = "WoWInfo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        //Instalado o pacote Google Cloud Message, pelo Components
+        static MainActivity instance = null;
+
+        //Expõe a instancia atual para poder executar na thread principal
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
+            instance = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -21,6 +37,32 @@ namespace WoWInfo.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+            try
+            {
+                //instalado a library GcmClient e verificamos se está tudo Ok
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception ex)
+            {
+                CreateAndShowDialog(ex.Message, "Error");
+            }
+        }
+
+        private void CreateAndShowDialog(string message, string title)
+        {
+            var builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
         }
     }
 }
--- WoWInfo/WoWInfo.Android/Resources/Resource.Designer.cs
+++ WoWInfo/WoWInfo.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace WoWInfo.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Gcm.Client.Resource.String.library_name = global::WoWInfo.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::WoWInfo.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3020,6 +3021,9 @@ namespace WoWInfo.Droid
 			
 			// aapt resource value: 0x7f080025
 			public const int character_counter_pattern = 2131230757;
+			
+			// aapt resource value: 0x7f080026
+			public const int library_name = 2131230758;
 			
 			// aapt resource value: 0x7f080000
 			public const int mr_button_content_description = 2131230720;
